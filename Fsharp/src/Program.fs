077d8bf8dfd43b3e90bc701a4bd6ee49459c1553
SimonNyvall <simon.k.nyvall@proton.me>
2024-06-16 20:57:36 +0200

    feat: add fsharp solution

---
+++ Fsharp/src/Program.fs
@@ -0,0 +1,45 @@
+﻿module WordCounter
+
+open System
+open System.IO
+
+type SearchQuery =
+    { FilePath: string; SearchWord: string }
+
+
+let parseArguments (args: string[]) : Result<SearchQuery, string> =
+    let doesFileExist (path: string) = File.Exists path
+
+    match args with
+    | args when args.Length <> 1 -> Error "Invalid number of arguments"
+    | args when not <| doesFileExist args.[0] -> Error "File does not exist"
+    | args ->
+        Ok
+            { FilePath = args.[0]
+              SearchWord = Path.GetFileNameWithoutExtension args.[0] }
+
+
+let getTotalWordAccurences (query: SearchQuery) : int =
+    let fileContent = File.ReadLines query.FilePath
+
+    let totalWordHits (words: string array) =
+        words
+        |> Array.sumBy (fun word ->
+            match (word = query.SearchWord) with
+            | true -> 1
+            | false -> 0)
+
+    fileContent
+    |> Seq.sumBy (fun line -> line.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries) |> totalWordHits)
+
+
+[<EntryPoint>]
+let main args =
+    match parseArguments args with
+    | Ok query ->
+        let totalWordHits = getTotalWordAccurences query
+
+        printfn "Word '%s' appears %d times in the file '%s'" query.SearchWord totalWordHits query.FilePath
+    | Error message -> printfn "Error: %s" message
+
+    0
